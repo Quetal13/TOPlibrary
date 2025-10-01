3d42858f8d9499a2ebe01efd8615279af2b85793
Quetal13 <135510813+Quetal13@users.noreply.github.com>
2025-10-01 04:59:11 +0400

    .

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const booksContainer = document.getElementById('books-container');
 //Array of book objects
 let myLibrary = [];
 
-
 //This function is a constructor for make book objects
 function Book(title, author, pages, readState) {
     this.title = title;
@@ -40,7 +39,9 @@ function cardGenerator(book) {
     const bookTitle = document.createElement("h2");
     const bookAuthor = document.createElement("span");
     const bookPages = document.createElement("span");
+    const readStateCheckBox = document.createElement("input");
     const bookReadState = document.createElement("span");
+    const readStateContainer = document.createElement("div");
     const deleteBook = document.createElement('button');
 
     bookCard.id = book.id;
@@ -48,21 +49,42 @@ function cardGenerator(book) {
 
     bookTitle.textContent = book.title;
     
-    bookAuthor.textContent = book.author;
+    bookAuthor.textContent = `Author: ${book.author}`;
 
-    bookPages.textContent = book.pages;
+    bookPages.textContent = `Pages: ${book.pages}`;
 
-    bookReadState.textContent = book.readState;
+    readStateCheckBox.type = 'checkbox';
+
+    readStateCheckBox.id = `read-status=${book.id}`
+
+    if (book.readState === true) {
+        bookReadState.textContent = ' read';
+    } else {
+        bookReadState.textContent = ' not read yet';
+    };
 
     deleteBook.textContent = 'Delete Book';
+
+    readStateContainer.appendChild(readStateCheckBox);
+    readStateContainer.appendChild(bookReadState);
 
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookAuthor);
     bookCard.appendChild(bookPages);
-    bookCard.appendChild(bookReadState);
+    bookCard.appendChild(readStateContainer);
     bookCard.appendChild(deleteBook);
 
     booksContainer.appendChild(bookCard);
+
+    readStateCheckBox.addEventListener('change', function (){
+        const targetBook = myLibrary.find(item => item.id === book.id)
+
+        if (targetBook) {
+            targetBook.toggleReadState();
+
+            renderBooks();
+        };
+    });
 
     //Event listener for deleteBook button
 
@@ -94,7 +116,7 @@ submitButton.addEventListener('click', function(event) {
     let newBookTitle = titleInput.value;
     let newBookAuthor = authorInput.value;
     let newBookPages = pagesInput.value;
-    let newBookRead = '';
+    let newBookRead = readInput.checked;
 
     addBookToLibrary(newBookTitle, newBookAuthor, newBookPages, newBookRead);
     event.preventDefault();
@@ -108,7 +130,10 @@ function deleteBookFromLibrary(bookId) {
     renderBooks();
 };
 
+Book.prototype.toggleReadState = function () {
+    this.readState = !this.readState;
+};
 
-addBookToLibrary("Star Wars: Episode II", "George Lucas", "620 pages", "not read yet");
+addBookToLibrary("Star Wars: Episode II", "George Lucas", "620 pages", false);
 
 console.log(myLibrary);
